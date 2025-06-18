b877640ea83041ceadcbe0bb12143f7cbddb871a
azadsingh99 <azadsingh1934278@gmail.com>
2025-06-18 19:03:47 +0530

    Removed the testimonials

--- app/page.tsx
+++ app/page.tsx
@@ -684,7 +684,7 @@ export default function LandingPage() {
           </div>
         </section>
 
-        {/* Testimonials Section */}
+        {/* Testimonials Section
         <section id="testimonials" className="w-full py-20 md:py-32">
           <div className="container px-4 md:px-6">
             <motion.div
@@ -779,7 +779,7 @@ export default function LandingPage() {
               ))}
             </div>
           </div>
-        </section>
+        </section> */}
 
         {/* FAQ Section */}
         <section id="faq" className="w-full py-20 md:py-32">
